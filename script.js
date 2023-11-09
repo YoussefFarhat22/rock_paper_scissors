8136bd26d3f3e9b4c7cd9ab6ccb024f719cc5e38
YoussefFarhat22 <contactyousseffarhat@gmail.com>
2023-11-09 16:17:48 +0100

    making a UI for our Rock Paper Scissors game

--- script.js
+++ script.js
@@ -1,46 +1,54 @@
-// Set computer Choice
-function getComputerChoice(){
- let choices = ["paper","scissors","rock"];
- return choices[Math.floor(Math.random() * choices.length)]
-}
+const buttonsSelection = document.querySelectorAll('[data-selection]');
+const playerScore = document.querySelector('[data-your-score]');
+const computerScore = document.querySelector('[data-computer-score]');
+const result = document.querySelector('.result');
 
-let computerScore = 0;
-let playerSCore = 0;
+let myScore = 0;
+let opponentScore = 0;
 
-
-
-// play one round function 
-function playerRound(){
- playerSelection = prompt('Enter your selection').toLocaleLowerCase();
- computerChoice = getComputerChoice();
- if((playerSelection === 'rock' && computerChoice === "scissors")  || (playerSelection === 'scissors' && computerChoice  === 'paper' ) || (playerSelection === 'paper' && computerChoice  === 'rock' ))
- {
-     playerSCore++;
-     return "You win!"
- }
- else if ((playerSelection === 'scissors' && computerChoice === "rock")  || (playerSelection === 'paper' && computerChoice  === 'scissors' ) || (playerSelection === 'rock' && computerChoice  === 'paper' ))
- {
-     computerScore++;
-     return "You lose!"
- }
- else{
-     return "draw!"
- }
-}
+buttonsSelection.forEach((btn) => {
+   btn.addEventListener('click',(e) => {
+    playerRound(btn.dataset.selection)
+   })
+})
 
 
-// play 5 round function 
-function game(){
- for(let i = 0;i<5;i++){
-     playerRound()
+// Show the winner
+function displayWinner(score){
+ if(score >= 5) {
+  result.textContent = `${score === myScore ? 'You win the game 🎉' : 'computer win the game'}`
+  resetGame()
+  }
  }
- if(playerSCore>=3){
-     console.log('Congratulation! You win!!')
+
+ function resetGame(){
+   myScore = 0
+   opponentScore = 0
+   playerScore.textContent = 0
+   computerScore.textContent = 0
  }
- else if (computerChoice >= 3){
-     console.log("You lose!")
+ 
+ // play one round function 
+ function playerRound(playerSelection){
+  computerChoice = getComputerChoice();
+  if((playerSelection === 'rock' && computerChoice === "scissors")  || (playerSelection === 'scissors' && computerChoice  === 'paper' ) || (playerSelection === 'paper' && computerChoice  === 'rock' ))
+  {
+   playerScore.textContent = `${++myScore}`;
+   displayWinner(myScore)
+  }
+  else if ((playerSelection === 'scissors' && computerChoice === "rock")  || (playerSelection === 'paper' && computerChoice  === 'scissors' ) || (playerSelection === 'rock' && computerChoice  === 'paper' ))
+  {
+   computerScore.textContent = `${++opponentScore}`;
+   displayWinner(opponentScore)
+  }
+  else{
+   console.log("draw!")
+  }
  }
- else{
-     console.log("Draw");
+ 
+ // Set computer Choice
+ function getComputerChoice(){
+  let choices = ["paper","scissors","rock"];
+  return choices[Math.floor(Math.random() * choices.length)]
  }
-}
+
